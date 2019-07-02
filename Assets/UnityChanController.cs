382e9179b15c20602a16a7d090dd0af75855852e
rnurakami <mariamanzini8@gmail.com>
2019-07-02 20:37:08 +0900

    Add running animation to unity-chan

---
+++ Assets/UnityChanController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityChanController : MonoBehaviour
+{
+
+    private Animator myAnimator;
+
+    void Start()
+    {
+        this.myAnimator = GetComponent<Animator>();
+        this.myAnimator.SetFloat("Speed", 1);
+
+    }
+
+    void Update()
+    {
+
+    }
+}
