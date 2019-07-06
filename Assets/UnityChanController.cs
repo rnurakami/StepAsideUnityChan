559e4b4e673a3b149a8f0ee61b3768592093de08
rnurakami <mariamanzini8@gmail.com>
2019-07-06 11:03:24 +0900

    Make unity-chan can move left and right on the road

--- Assets/UnityChanController.cs
+++ Assets/UnityChanController.cs
@@ -11,6 +11,10 @@ public class UnityChanController : MonoBehaviour
 
     private float forwardForce = 800.0f;
 
+    private float turnForce = 500.0f;
+
+    private float movableRange = 3.4f;
+
     void Start()
     {
         this.myAnimator = GetComponent<Animator>();
@@ -23,6 +27,15 @@ public class UnityChanController : MonoBehaviour
     void Update()
     {
         this.myRidgebody.AddForce(this.transform.forward * this.forwardForce);
+
+        if (Input.GetKey(KeyCode.LeftArrow) && -this.movableRange < this.transform.position.x)
+        {
+            this.myRidgebody.AddForce(-this.turnForce, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow) && this.movableRange > this.transform.position.x)
+        {
+            this.myRidgebody.AddForce(this.turnForce, 0, 0);
+        }
 
     }
 }
