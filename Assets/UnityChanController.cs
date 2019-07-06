8a5b02126ed155fe547093db3e0a8a888f16f102
rnurakami <mariamanzini8@gmail.com>
2019-07-06 10:13:53 +0900

    Make unity-chan move forward

--- Assets/UnityChanController.cs
+++ Assets/UnityChanController.cs
@@ -7,15 +7,22 @@ public class UnityChanController : MonoBehaviour
 
     private Animator myAnimator;
 
+    private Rigidbody myRidgebody;
+
+    private float forwardForce = 800.0f;
+
     void Start()
     {
         this.myAnimator = GetComponent<Animator>();
         this.myAnimator.SetFloat("Speed", 1);
 
+        this.myRidgebody = GetComponent<Rigidbody>();
+
     }
 
     void Update()
     {
+        this.myRidgebody.AddForce(this.transform.forward * this.forwardForce);
 
     }
 }
